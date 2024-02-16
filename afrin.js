a161fcd1b5ef37af617460f0020769804a736fe8
Afrin <154747276+afrin823@users.noreply.github.com>
2024-02-16 11:21:14 +0600

    Add files via upload

---
+++ afrin.js
@@ -0,0 +1,22 @@
+//  --- 5 number ---
+let firstAssignment = 58;
+let secondAssignment = 55;
+
+let totalResult = firstAssignment + secondAssignment;
+
+console.log(totalResult);
+
+//  -- 7 number -- 
+var nickName = 'Afrin';
+var nickName = 'Sabikun';
+
+console.log(nickName);
+
+let firstName = 'Sabikun Nahar'
+let lastName = 'Afrin';
+
+let fullName = firstName + '' + lastName;
+console.log(fullName);
+
+
+
